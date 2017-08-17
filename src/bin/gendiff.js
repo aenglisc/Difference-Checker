6760af2da45da65afd6904109b3efd85831210b2
Roman Pushkov <pushkovroman@me.com>
2017-08-17 14:31:24 +0300

    added gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,11 @@
 #!/usr/local/bin/node
+import program from 'commander';
+import json from '../../package.json';
+
+program
+  .version(json.version)
+  .description(json.description)
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
 
 console.log('working');
