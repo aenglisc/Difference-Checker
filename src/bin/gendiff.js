967f8fc4a4e134c7827c050ec0e769112f09ec29
Roman Pushkov <pushkovroman@me.com>
2017-08-17 21:52:15 +0300

    added action to binary

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/local/bin/node
 import program from 'commander';
 import json from '../../package.json';
+import gendiff from '..';
 
 program
   .version(json.version)
   .description(json.description)
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(gendiff(firstConfig, secondConfig));
+  })
   .parse(process.argv);
