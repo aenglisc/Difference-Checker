67cca6c327e23b8f933a7ee9213cb6409048a41b
Roman Pushkov <pushkovroman@me.com>
2017-08-17 15:03:58 +0300

    added .option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,7 @@ import json from '../../package.json';
 program
   .version(json.version)
   .description(json.description)
+  .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .parse(process.argv);
 
