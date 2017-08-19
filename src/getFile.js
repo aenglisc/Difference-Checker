0de88ef9b433aa9a992ebb9da468a8875273e231
Roman Pushkov <pushkovroman@me.com>
2017-08-19 15:07:55 +0300

    merged parseFile, getFile into getConfig

--- src/getFile.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default (filePath) => {
-  const data = fs.readFileSync(filePath, 'utf8');
-  const extension = path.extname(filePath, 'utf8');
-  return { data, extension };
-};
