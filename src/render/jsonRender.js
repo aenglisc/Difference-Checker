155f724636baa481c2b0bdb46775d99ce2a2dd03
Roman Pushkov <pushkovroman@me.com>
2017-08-21 02:11:33 +0300

    rewrote json renderer

--- src/render/jsonRender.js
+++ src/render/jsonRender.js
@@ -1,26 +1,9 @@
-const jsonRender = tree => tree.reduce((acc, node) => {
-  const { key, type, oldValue, newValue, children } = node;
-
-  switch (type) {
-
-    case 'nested':
-      return { ...acc, [key]: jsonRender(children) };
-
-    case 'created':
-      return { ...acc, [key]: { type, newValue } };
-
-    case 'removed':
-      return { ...acc, [key]: { type, oldValue } };
-
-    case 'changed':
-      return { ...acc, [key]: { type, oldValue, newValue } };
-
-    case 'unchanged':
-      return { ...acc, [key]: { type, oldValue } };
-
-    default:
-      return acc;
+export default tree => JSON.stringify(tree, (name, val) => {
+  if (val instanceof Array && val.length < 1) {
+    return undefined;
   }
-}, {});
-
-export default tree => JSON.stringify(jsonRender(tree), null, ' ');
+  if (val === 'nested') {
+    return undefined;
+  }
+  return val;
+}, 2);
