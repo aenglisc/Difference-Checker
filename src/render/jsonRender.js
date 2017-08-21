9efafc65db9276fc6ba6be54ee9f9578de54d132
Roman Pushkov <pushkovroman@me.com>
2017-08-21 03:14:25 +0300

    json render update

--- src/render/jsonRender.js
+++ src/render/jsonRender.js
@@ -1,9 +1,15 @@
-export default tree => JSON.stringify(tree, (name, val) => {
-  if (val instanceof Array && val.length < 1) {
+export default tree => JSON.stringify(tree, function replacer(name, val) {
+  if (this.type === 'nested') {
+    if (name === 'oldValue' || name === 'newValue' || name === 'type') {
+      return undefined;
+    }
+  } else if (name === 'children') {
     return undefined;
   }
-  if (val === 'nested') {
-    return undefined;
+  if (this.type === 'unchanged') {
+    if (name === 'newValue') {
+      return undefined;
+    }
   }
   return val;
 }, 2);
